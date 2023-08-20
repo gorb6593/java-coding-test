fcbd4857c59494b8038da76d4edd551043a4bde1
gorb6593 <gorb6593@gmail.com>
2023-08-20 23:50:58 +0900

    테스트 첫번째 문제

---
+++ src/main/java/org/example/level1/first_test001_0001.java
@@ -0,0 +1,20 @@
+package org.example.level1;
+
+import java.util.Scanner;
+
+public class first_test001_0001 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+
+        int sum = 0;
+        for ( int i = 0; i < sNum.length(); i++ ) {
+            sum += cNum[i] - '0';
+        }
+        System.out.println(sum);
+    }
+}
+
