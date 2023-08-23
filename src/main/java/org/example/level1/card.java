ba4fcccd9bc00fcee9931fcb78a2926c6797ea7f
gorb6593 <gorb6593@gmail.com>
2023-08-23 23:55:34 +0900

    카드정렬 문젬나 풀이

---
+++ src/main/java/org/example/level1/card.java
@@ -0,0 +1,33 @@
+package org.example.level1;
+
+public class card {
+
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+
+        int card1Index = 0;
+        int card2Index = 0;
+
+        for (int i = 0; i < goal.length; i++) {
+            if(card1Index < cards1.length && goal[i].equals(cards1[card1Index])) {
+                card1Index++;
+                continue;
+            }
+
+            if(card2Index < cards2.length && goal[i].equals(cards2[card2Index])) {
+                card2Index++;
+                continue;
+            }
+
+            answer = "No";
+            return answer;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        card card = new card();
+//        String[] arr = 
+//        System.out.println(card.solution());
+    }
+}
