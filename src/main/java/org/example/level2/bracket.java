8d0ca6203c8cbbbf825b3e6c94780f54909838af
gorb6593 <gorb6593@gmail.com>
2023-08-30 23:53:05 +0900

    스택문제

---
+++ src/main/java/org/example/level2/bracket.java
@@ -0,0 +1,30 @@
+package org.example.level2;
+
+import org.example.level1.card;
+
+import java.util.Stack;
+
+public class bracket {
+    public class Solution {
+        boolean solution(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    stack.push('(');
+                } else if (s.charAt(i) == ')') {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    stack.pop();
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+    public static void main(String[] args) {
+        bracket bracket = new bracket();
+
+        String s = "()()";
+        System.out.println(bracket);
+    }
+}
