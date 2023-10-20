74f9e1e5914df1cc910f67f20051685529f50b6e
gorb6593 <gorb6593@gmail.com>
2023-10-20 22:26:59 +0900

    데일리문제풀이

--- src/main/java/org/example/bacjoon/_25314.java
+++ src/main/java/org/example/bacjoon/_25314.java
@@ -1,9 +1,17 @@
 package org.example.bacjoon;
 
+import java.util.Scanner;
+
 public class _25314 {
   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double a = sc.nextInt();
 
+    String answer = "";
+    for(int i=0; i<Math.ceil(a/4); i++){
+       answer += "long ";
+    }
+    answer = answer + "int";
+    System.out.println(answer);
   }
-
-
 }
