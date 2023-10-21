13b740d3efdfa6cd1ce7614a876b90cdb2da7752
gorb6593 <gorb6593@gmail.com>
2023-10-21 22:27:23 +0900

    데일리문제풀이

--- src/main/java/org/example/bacjoon/_25314.java
+++ src/main/java/org/example/bacjoon/_25314.java
@@ -2,6 +2,7 @@ package org.example.bacjoon;
 
 import java.util.Scanner;
 
+//https://harrystroy.tistory.com/manage/posts/
 public class _25314 {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
