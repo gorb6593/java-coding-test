78eb440bde57cfb241df3ca2bdacfccae3e29b5b
gorb6593 <gorb6593@gmail.com>
2023-10-25 22:57:10 +0900

    백준문제풀이

---
+++ src/main/java/org/example/bacjoon/_27866.java
@@ -0,0 +1,14 @@
+package org.example.bacjoon;
+
+import java.util.Scanner;
+
+public class _27866 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    int a = sc.nextInt() -1;
+
+    System.out.println(str.charAt(a));
+
+  }
+}
