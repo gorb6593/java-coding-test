53ee40b9ada23a280239f7008a0f2fae0a4aa58f
gorb6593 <gorb6593@gmail.com>
2023-10-27 22:54:28 +0900

    블로그 글주소

--- src/main/java/org/example/bacjoon/_2743.java
+++ src/main/java/org/example/bacjoon/_2743.java
@@ -1,7 +1,7 @@
 package org.example.bacjoon;
 
 import java.util.Scanner;
-
+//https://harrystroy.tistory.com/21
 public class _2743 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
