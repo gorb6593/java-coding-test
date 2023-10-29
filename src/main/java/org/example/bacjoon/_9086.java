c6c23103fbcd802df7c15772b5ffdaa61b8a9259
gorb6593 <gorb6593@gmail.com>
2023-10-30 00:01:30 +0900

    코드수정

--- src/main/java/org/example/bacjoon/_9086.java
+++ src/main/java/org/example/bacjoon/_9086.java
@@ -6,11 +6,10 @@ public class _9086 {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    String name[] = new String[n];
 
     for(int i=0;i<n;i++) {
       String s = sc.next();
-      System.out.println(s.charAt(0));
+      System.out.print(s.charAt(0));
       System.out.println(s.charAt(s.length()-1));
     }
   }
