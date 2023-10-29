5fd53387b3347929b12a62e625296d80fdcfbd73
gorb6593 <gorb6593@gmail.com>
2023-10-29 23:43:19 +0900

    데일리 문제풀이

--- src/main/java/org/example/bacjoon/_9086.java
+++ src/main/java/org/example/bacjoon/_9086.java
@@ -1,7 +1,17 @@
 package org.example.bacjoon;
 
+import java.util.Scanner;
+
 public class _9086 {
   public static void main(String[] args){
-    System.out.println("문제 셋팅");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    String name[] = new String[n];
+
+    for(int i=0;i<n;i++) {
+      String s = sc.next();
+      System.out.println(s.charAt(0));
+      System.out.println(s.charAt(s.length()-1));
+    }
   }
 }
