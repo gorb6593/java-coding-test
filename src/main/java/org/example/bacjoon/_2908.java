45ab9a9e47a3ab13e1c4281695a2cec29aba608c
gorb6593 <gorb6593@gmail.com>
2023-11-02 23:20:52 +0900

    문제풀이

--- src/main/java/org/example/bacjoon/_2908.java
+++ src/main/java/org/example/bacjoon/_2908.java
@@ -1,7 +1,19 @@
 package org.example.bacjoon;
 
+import java.util.Scanner;
+
 public class _2908 {
   public static void main(String[] args) {
     //stringbuilder 사용해서 문제풀기
+
+    Scanner in = new Scanner(System.in);
+
+    int A = in.nextInt();
+    int B = in.nextInt();
+
+    A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+    B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+
+    System.out.print(A > B ? A : B);
   }
 }
