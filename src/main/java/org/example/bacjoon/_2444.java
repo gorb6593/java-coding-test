f0a854bb6779087e22688c262c3518515a9b43e1
gorb6593 <gorb6593@gmail.com>
2023-11-08 23:31:07 +0900

    데일리문제풀이

--- src/main/java/org/example/bacjoon/_2444.java
+++ src/main/java/org/example/bacjoon/_2444.java
@@ -2,6 +2,8 @@ package org.example.bacjoon;
 
 public class _2444 {
   public static void main(String[] args) {
-    System.out.println("문제세팅");
+
   }
+
+
 }
---
+++ src/main/java/org/example/programmers/_12935.java
@@ -0,0 +1,16 @@
+package org.example.programmers;
+
+public class _12935 {
+  public static void main(String[] args) {
+    int test_arr[] = {1,2};
+
+  }
+
+
+  public class Solution {
+    public int[] solution(int[] arr) {
+      int[] answer = {};
+      return answer;
+    }
+  }
+}
