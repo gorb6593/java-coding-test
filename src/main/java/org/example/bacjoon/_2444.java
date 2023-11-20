5f9b31eebc4ee40f5adf745b5025604512963a91
gorb6593 <gorb6593@gmail.com>
2023-11-20 23:55:48 +0900

    데일리 문제풀이

--- src/main/java/org/example/bacjoon/_2444.java
+++ src/main/java/org/example/bacjoon/_2444.java
@@ -1,8 +1,40 @@
 package org.example.bacjoon;
 
+import java.util.Scanner;
+
 public class _2444 {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    int n = sc.nextInt();
+
+    for(int i=1; i<=n; i++) {
+      for (int j=0; j<n-i; j++) {
+        //
+        System.out.print(" ");
+      }
+      for(int j=0; j<i; j++) {
+        System.out.print("*");
+      }
+      for(int j=0; j<i-1; j++) {
+        System.out.print("*");
+      }
+      //끝나면 줄바꿈
+      System.out.println();
+    }
+
+    for(int i=1; i<n; i++) {
+      for (int j=0; j<i; j++) {
+        System.out.print(" ");
+      }
+      for(int j=0; j<n-i; j++) {
+        System.out.print("*");
+      }
+      for(int j=0; j<n-i-1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 
 
