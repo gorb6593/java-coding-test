807e61500fa4ed6a802e61e35d755dcee5de2a87
gorb6593 <gorb6593@gmail.com>
2023-11-21 00:04:39 +0900

    데일리 문제풀이

--- src/main/java/org/example/bacjoon/_2444.java
+++ src/main/java/org/example/bacjoon/_2444.java
@@ -10,13 +10,15 @@ public class _2444 {
 
     for(int i=1; i<=n; i++) {
       for (int j=0; j<n-i; j++) {
-        //
+        //해당 줄이 n보다 작으면 띄우기
         System.out.print(" ");
       }
       for(int j=0; j<i; j++) {
+        //해당 줄의 중간 포함 왼쪽부분
         System.out.print("*");
       }
       for(int j=0; j<i-1; j++) {
+        //해당 줄의 중간 불포함 오른쪽부분
         System.out.print("*");
       }
       //끝나면 줄바꿈
