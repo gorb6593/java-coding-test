d0632b056d317387fd9f8f9dfcb12db07f986971
gorb6593 <gorb6593@gmail.com>
2023-12-04 23:03:13 +0900

    데일리문제풀이

--- src/main/java/org/example/bacjoon/_10988.java
+++ src/main/java/org/example/bacjoon/_10988.java
@@ -1,22 +1,24 @@
 package org.example.bacjoon;
+
+import java.util.Scanner;
+
 //티스토리 연동해야함
 public class _10988 {
   public static void main(String[] args){
-    int num1 =0;
-    int num2 =1;
-    int sum =1;
-    int answer = 0;
-    for(int i=0; i<166; i++){
-      System.out.print(sum+" ");
-      answer += sum;
-      if(sum>100){
-        System.out.println("오니?"+num2);
-        System.out.println(answer-num2);
+    Scanner sc = new Scanner(System.in);
+
+    String str = sc.next();
+    sc.close();
+
+    int len = str.length();
+    int answer = 1;
+
+    for(int i=0; i<len/2; i++){
+      if(str.charAt(i) != str.charAt(len-1-i)){
+        answer = 0;
         break;
       }
-      sum=num1+num2;
-      num1=num2;
-      num2=sum;
     }
+    System.out.println(answer);
   }
 }
