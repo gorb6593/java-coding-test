682c07b3dcf7591cbf1648bcd060c879a0ef3136
gorb6593 <gorb6593@gmail.com>
2023-12-10 23:55:40 +0900

    데일리문제세팅

---
+++ src/main/java/org/example/bacjoon/_1157.java
@@ -0,0 +1,11 @@
+package org.example.bacjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _1157 {
+  public static void main(String args[]){
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  }
+
+}
