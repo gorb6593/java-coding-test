b217710bbfc3d2faaf424a22d92e2d5eaa914895
gorb6593 <gorb6593@gmail.com>
2023-12-12 23:44:28 +0900

    daily coding test

--- src/main/java/org/example/bacjoon/_1157.java
+++ src/main/java/org/example/bacjoon/_1157.java
@@ -1,11 +1,38 @@
 package org.example.bacjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _1157 {
-  public static void main(String args[]){
+  public static void main(String args[]) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String str = br.readLine().toUpperCase();
+    boolean flag = false;
+    int maxValue = 0;
+    int maxIndex = 0;
+
+    int[] Arr = new int[26];
+    for (int i = 0; i < str.length(); i++) {
+      Arr[str.charAt(i) - 'A']++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+      int compare = Arr[i] - maxValue;
+      if (compare > 0) {
+        maxValue = Arr[i];
+        flag = false;
+        maxIndex = i;
+      } else if (compare == 0) {
+        flag = true;
+      }
+    }
+    if (flag) {
+      System.out.println("?");
+    } else {
+      System.out.println((char) (maxIndex + 'A'));
+    }
   }
 
 }
