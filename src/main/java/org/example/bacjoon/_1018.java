97c36ee591d89e3406fcba1d4d945b4a80cc794d
gorb6593 <gorb6593@gmail.com>
2023-12-27 00:19:42 +0900

    coding test

--- src/main/java/org/example/bacjoon/_1018.java
+++ src/main/java/org/example/bacjoon/_1018.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class _1018 {
+
+  public static int getSolusion(int startRow, int startCol, String[] board) {
+    String[] orBoard = {"WBWBWBWB", "BWBWBWBW"};
+    int whiteSol = 0;
+    for (int i=0; i<8; i++) {
+      int row = startRow + i;
+      for (int j=0; j<8; j++) {
+        int col = startCol + j;
+      }
+    }
+    return Math.min(whiteSol, 64 - whiteSol);
+  }
   public static void main(String[] args) {
 /*
 10 13
@@ -26,13 +38,31 @@ WWWWWWWWWWBWB
     sc.nextLine();
 
     String[] board = new String[row];
-    System.out.println("row::"+row);
-    System.out.println("col::"+col);
+
 
     for(int i=0; i<row; i++) {
       board[i] = sc.nextLine();
     }
-    System.out.println(Arrays.toString(board));
+    //테스트
+    //System.out.println("row::"+row);
+    //System.out.println("col::"+col);
+    //System.out.println(Arrays.toString(board));
+
+    //체스판 자르기
+    int sol = Integer.MAX_VALUE;
+
+    for (int i=0; i < row-8; i++) {
+      for (int j=0; j < col -8; j++) {
+        int curSol = getSolusion(i,j,board);
+
+        if(sol > curSol) {
+          sol = curSol;
+        }
+      }
+    }
+
+    System.out.println(sol);
+    sc.close();
 
 
   }
