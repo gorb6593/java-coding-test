718a52513b6478fc2fae8450bb8536ec696c8f39
gorb6593 <gorb6593@gmail.com>
2024-01-01 23:39:45 +0900

    coding test

--- src/main/java/org/example/bacjoon/_1436.java
+++ src/main/java/org/example/bacjoon/_1436.java
@@ -1,9 +1,25 @@
 package org.example.bacjoon;
 
+import java.util.Scanner;
+
 public class _1436 {
   public static void main(String[] args) {
     /*
      0< N < 10000 자연수
      */
+    Scanner in = new Scanner(System.in);
+    int a = in.nextInt();
+
+    int num = 666;
+    int count = 1;
+
+    while(count != a) {
+      num++;
+      //System.out.println("num ::" + num + ", count :: "+count);
+      if(String.valueOf(num).contains("666")) {
+        count++;
+      }
+    }
+    System.out.println(num);
   }
 }
