50d4c91845bc4b51dc28b947b745451cc5356671
gorb6593 <gorb6593@gmail.com>
2024-01-03 00:12:30 +0900

    coding test

---
+++ src/main/java/org/example/bacjoon/_25305.java
@@ -0,0 +1,2 @@
+package org.example.bacjoon;public class _25305 {
+}
--- src/main/java/org/example/bacjoon/_2587.java
+++ src/main/java/org/example/bacjoon/_2587.java
@@ -1,2 +1,32 @@
-package org.example.bacjoon;public class _2587 {
+package org.example.bacjoon;
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class _2587 {
+  public static void main(String[] args) {
+        /*
+        10
+40
+30
+60
+30
+         */
+    Scanner sc = new Scanner(System.in);
+    int answer = 0;
+    int sum = 0;
+    int[] arr = new int[5];
+    for (int i=0; i<5; i++) {
+      arr[i] = sc.nextInt();
+    }
+    Arrays.sort(arr);
+    for (int i=0; i<5; i++){
+      sum += arr[i];
+      if(i == 2) {
+        answer = arr[i];
+      }
+    }
+    System.out.println(sum/5);
+    System.out.println(answer);
+
+  }
 }
