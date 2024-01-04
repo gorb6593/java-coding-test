9f31ce9113c9f3eaef7dd110204fb62fb0ced3f4
gorb6593 <gorb6593@gmail.com>
2024-01-04 23:13:45 +0900

    coding test

--- src/main/java/org/example/bacjoon/_25305.java
+++ src/main/java/org/example/bacjoon/_25305.java
@@ -1,2 +1,29 @@
-package org.example.bacjoon;public class _25305 {
+package org.example.bacjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _25305 {
+  public static void main(String[] args) {
+    /*
+  5 2
+100 76 85 93 98
+   */
+    Scanner sc = new Scanner(System.in);
+
+    int count = sc.nextInt();
+    int cutLine = sc.nextInt();
+
+    int[] arr = new int[count];
+
+    for (int i=0; i<count; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    Arrays.sort(arr);
+    //System.out.println(Arrays.toString(arr));
+    System.out.println(arr[arr.length - cutLine]);
+  }
+
+
 }
