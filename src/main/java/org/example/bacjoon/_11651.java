fbdc8f9d4d3ccbf16884f0134b2243f0d938c681
gorb6593 <gorb6593@gmail.com>
2024-01-08 23:26:43 +0900

    coding test

---
+++ src/main/java/org/example/bacjoon/_11651.java
@@ -0,0 +1,22 @@
+package org.example.bacjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11651 {
+  public static void main(String[] args) throws IOException {
+
+    //입력받기
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    //스프링빌러 생성
+    StringBuilder sb = new StringBuilder();
+
+    //첫 번째 라인 만큼 반복
+    int count = Integer.parseInt(br.readLine());
+
+    //2차원 배열 생성
+    int[][] arr = new int[count][2];
+  }
+}
