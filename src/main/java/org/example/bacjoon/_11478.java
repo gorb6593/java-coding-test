140df2606181af642abf17ccbd237a8d79aa699e
gorb6593 <gorb6593@gmail.com>
2024-01-20 23:52:24 +0900

    coding test

---
+++ src/main/java/org/example/bacjoon/_11478.java
@@ -0,0 +1,28 @@
+package org.example.bacjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _11478 {
+  public static void main(String[] args) throws IOException {
+    //입력받기
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String value = br.readLine();
+    //System.out.println("value.length() = " + value.length());
+    Set<String> set = new HashSet<>();
+    for (int i=0; i<value.length(); i++) {
+      for (int j=i+1; j<=value.length(); j++) {
+        set.add(value.substring(i, j));
+      }
+    }
+
+    //System.out.println("set = " + set);
+    System.out.println(set.size());
+
+
+  }
+
+}
