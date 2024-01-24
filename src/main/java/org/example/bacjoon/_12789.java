92e5b768fd74ae36e53fca3b271a4287ceba1c31
haekyu <gorb6593@gmail.com>
2024-01-24 23:18:54 +0900

    데일리 코딩테스트

--- src/main/java/org/example/bacjoon/_12789.java
+++ src/main/java/org/example/bacjoon/_12789.java
@@ -21,26 +21,49 @@ public class _12789 {
 
         //현재 줄 큐로 생성
         Queue<Integer> queue = new LinkedList<>();
-        Stack<Integer> stack1 = new Stack<>();
+        
+        //대기열을 스택으로 생성
+        Stack<Integer> stack = new Stack<>();
         st = new StringTokenizer(br.readLine());
-        int value;
-        for (int i=0; i<count; i++) {
-            value = Integer.parseInt(st.nextToken());
-            queue.add(value);
-        }
-        int minValue = queue.peek();
 
         for (int i=0; i<count; i++) {
-            if(queue.peek() <= minValue) minValue = queue.peek();
-            stack1.push(queue.poll());
+            queue.offer(Integer.parseInt(st.nextToken()));
         }
 
-        for (int i=0; i<count; i++) {
-            if(stack1.peek() == minValue) {
-                stack1.pop();
+        int minValue = 1;
+
+        //현재줄(큐)이 빌때까지 진행
+        while (!queue.isEmpty()) {
+            //꺼내는 값이 가장 작은 수면 바로 꺼내고 +1
+            if (queue.peek() == minValue) {
+                queue.poll();
+                minValue++;
+
+                //대기열에 무엇인가가 있고 그 값이 가장 작은 수면 꺼내고 +1
+            } else if (!stack.isEmpty() && stack.peek() == minValue) {
+                stack.pop();
+                minValue++;
+
+                //지금 당장 나갈 사람에 해당 없으면 현재열 => 대기열(스택)에 추가
+            } else {
+                stack.push(queue.poll());
             }
         }
 
+        //현재줄은 다 빠졌고 대기줄 사람 체크
+        while (!stack.isEmpty()) {
+
+            //대기줄에서 꺼낸 값이 가장 작은 수면 꺼내고 +1
+            if (stack.peek() == minValue) {
+                stack.pop();
+                minValue++;
+            } else {
+                //아니면 정답
+                System.out.println("Sad");
+                return;
+            }
+        }
+        System.out.println("Nice");
 
     }
 }
