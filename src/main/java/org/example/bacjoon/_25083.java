bb4e24cee9c47e22ee640e168ec65863b3b6322c
haekyu <gorb6593@gmail.com>
2024-01-28 00:01:32 +0900

    데일리 코딩테스트1

---
+++ src/main/java/org/example/bacjoon/_25083.java
@@ -0,0 +1,15 @@
+package org.example.bacjoon;
+
+import java.io.IOException;
+
+public class _25083 {
+    public static void main(String[] args) {
+        System.out.println(
+                "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
