dcf3456191e574d8b1e8468b0ba348a90b349b2a
gorb6593 <88477706+gorb6593@users.noreply.github.com>
2024-01-28 22:06:00 +0900

    [Bronze II] Title: 진법 변환, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        String N = st.nextToken();
+        int value = Integer.parseInt(st.nextToken());
+
+        int tmp = 1;
+        int result = 0;
+
+
+        for (int i = N.length() - 1; i >= 0; i--) {
+            char c = N.charAt(i);
+            //System.out.println("c = " + c);
+            if (c >= 'A' && c <= 'Z') {
+                result += (c - 55) * tmp;
+            } else {
+                result += (c - 48) * tmp;
+            }
+            
+            //System.out.println("result = " + result);
+            tmp *= value;
+        }
+        System.out.println(result);
+    }
+}
+
+
