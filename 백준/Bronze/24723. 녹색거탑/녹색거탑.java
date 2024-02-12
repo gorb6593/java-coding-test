d4f3ab6f951fa992543d892ad7130bd011b78bce
gorb6593 <88477706+gorb6593@users.noreply.github.com>
2024-02-12 20:02:21 +0900

    [Bronze IV] Title: 녹색거탑, Time: 120 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double n = Double.parseDouble(br.readLine());
+        int answer = (int) Math.pow(2,n);
+        System.out.println(answer);
+    }
+}
